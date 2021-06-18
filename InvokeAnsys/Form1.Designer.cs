0818d35e1b854224a4f05500e40679de8d387a3b
浚陶 毛 <1048493621@qq.com>
2021-06-18 22:27:48 +0800

    添加项目文件。

---
+++ InvokeAnsys/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace InvokeAnsys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxAnsysFile = new System.Windows.Forms.TextBox();
+            this.textBoxAPDLFile = new System.Windows.Forms.TextBox();
+            this.buttonAnsysFileChoose = new System.Windows.Forms.Button();
+            this.buttonAPDLFileChoose = new System.Windows.Forms.Button();
+            this.buttonExcuteAnsys = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxJobName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxOutputDic = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonResultWorkDic = new System.Windows.Forms.Button();
+            this.textBoxResultFile = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxAnsysFile
+            // 
+            this.textBoxAnsysFile.Location = new System.Drawing.Point(156, 53);
+            this.textBoxAnsysFile.Name = "textBoxAnsysFile";
+            this.textBoxAnsysFile.Size = new System.Drawing.Size(541, 25);
+            this.textBoxAnsysFile.TabIndex = 0;
+            // 
+            // textBoxAPDLFile
+            // 
+            this.textBoxAPDLFile.Location = new System.Drawing.Point(156, 101);
+            this.textBoxAPDLFile.Name = "textBoxAPDLFile";
+            this.textBoxAPDLFile.Size = new System.Drawing.Size(541, 25);
+            this.textBoxAPDLFile.TabIndex = 0;
+            // 
+            // buttonAnsysFileChoose
+            // 
+            this.buttonAnsysFileChoose.Location = new System.Drawing.Point(763, 50);
+            this.buttonAnsysFileChoose.Name = "buttonAnsysFileChoose";
+            this.buttonAnsysFileChoose.Size = new System.Drawing.Size(75, 31);
+            this.buttonAnsysFileChoose.TabIndex = 1;
+            this.buttonAnsysFileChoose.Text = "浏览...";
+            this.buttonAnsysFileChoose.UseVisualStyleBackColor = true;
+            this.buttonAnsysFileChoose.Click += new System.EventHandler(this.buttonAnsysFileChoose_Click);
+            // 
+            // buttonAPDLFileChoose
+            // 
+            this.buttonAPDLFileChoose.Location = new System.Drawing.Point(763, 95);
+            this.buttonAPDLFileChoose.Name = "buttonAPDLFileChoose";
+            this.buttonAPDLFileChoose.Size = new System.Drawing.Size(75, 31);
+            this.buttonAPDLFileChoose.TabIndex = 1;
+            this.buttonAPDLFileChoose.Text = "浏览...";
+            this.buttonAPDLFileChoose.UseVisualStyleBackColor = true;
+            this.buttonAPDLFileChoose.Click += new System.EventHandler(this.buttonAPDLFileChoose_Click);
+            // 
+            // buttonExcuteAnsys
+            // 
+            this.buttonExcuteAnsys.Location = new System.Drawing.Point(76, 253);
+            this.buttonExcuteAnsys.Name = "buttonExcuteAnsys";
+            this.buttonExcuteAnsys.Size = new System.Drawing.Size(75, 31);
+            this.buttonExcuteAnsys.TabIndex = 1;
+            this.buttonExcuteAnsys.Text = "执行";
+            this.buttonExcuteAnsys.UseVisualStyleBackColor = true;
+            this.buttonExcuteAnsys.Click += new System.EventHandler(this.buttonExcuteAnsys_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ansys安装目录：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "APDL文件目录：";
+            // 
+            // textBoxJobName
+            // 
+            this.textBoxJobName.Location = new System.Drawing.Point(156, 147);
+            this.textBoxJobName.Name = "textBoxJobName";
+            this.textBoxJobName.Size = new System.Drawing.Size(145, 25);
+            this.textBoxJobName.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "工程名称：";
+            // 
+            // textBoxOutputDic
+            // 
+            this.textBoxOutputDic.Location = new System.Drawing.Point(156, 191);
+            this.textBoxOutputDic.Name = "textBoxOutputDic";
+            this.textBoxOutputDic.Size = new System.Drawing.Size(541, 25);
+            this.textBoxOutputDic.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "输出文件目录：";
+            // 
+            // buttonResultWorkDic
+            // 
+            this.buttonResultWorkDic.Location = new System.Drawing.Point(763, 191);
+            this.buttonResultWorkDic.Name = "buttonResultWorkDic";
+            this.buttonResultWorkDic.Size = new System.Drawing.Size(75, 31);
+            this.buttonResultWorkDic.TabIndex = 1;
+            this.buttonResultWorkDic.Text = "浏览...";
+            this.buttonResultWorkDic.UseVisualStyleBackColor = true;
+            this.buttonResultWorkDic.Click += new System.EventHandler(this.buttonResultWorkDic_Click);
+            // 
+            // textBoxResultFile
+            // 
+            this.textBoxResultFile.Location = new System.Drawing.Point(549, 144);
+            this.textBoxResultFile.Name = "textBoxResultFile";
+            this.textBoxResultFile.Size = new System.Drawing.Size(145, 25);
+            this.textBoxResultFile.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(422, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 15);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "结果文件名称：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 557);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonExcuteAnsys);
+            this.Controls.Add(this.buttonResultWorkDic);
+            this.Controls.Add(this.buttonAPDLFileChoose);
+            this.Controls.Add(this.buttonAnsysFileChoose);
+            this.Controls.Add(this.textBoxResultFile);
+            this.Controls.Add(this.textBoxOutputDic);
+            this.Controls.Add(this.textBoxJobName);
+            this.Controls.Add(this.textBoxAPDLFile);
+            this.Controls.Add(this.textBoxAnsysFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxAnsysFile;
+        private System.Windows.Forms.TextBox textBoxAPDLFile;
+        private System.Windows.Forms.Button buttonAnsysFileChoose;
+        private System.Windows.Forms.Button buttonAPDLFileChoose;
+        private System.Windows.Forms.Button buttonExcuteAnsys;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxJobName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxOutputDic;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonResultWorkDic;
+        private System.Windows.Forms.TextBox textBoxResultFile;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ InvokeAnsys/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace InvokeAnsys
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxAnsysFile.Text = @"C:\Program Files\ANSYS Inc\v202\ansys\bin\winx64\ANSYS202.exe";
+            textBoxAPDLFile.Text = @"C:\Users\Cratza\Desktop\C#\ANSYS_CSharp_Coding\ANSYS_CSharp_Coding\bin\Debug\ANSYS\demo.mac";
+            textBoxJobName.Text = "demo";
+            textBoxResultFile.Text = "result.out";
+            textBoxOutputDic.Text = @"C:\Users\Cratza\Desktop\C#\ANSYS_CSharp_Coding\ANSYS_CSharp_Coding\bin\Debug\ANSYS";
+        }
+
+        private void buttonAnsysFileChoose_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+           // openFileDialog.InitialDirectory = "E:\\";
+            openFileDialog.Filter = "Text Document（*.txt）|*.txt|All Files|*.*|我要显示的文件类型（*.exe）|*.exe";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.FilterIndex = 3;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.textBoxAnsysFile.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void buttonAPDLFileChoose_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            // openFileDialog.InitialDirectory = "E:\\";
+            openFileDialog.Filter = "Mac Document（*.txt）|*.txt|All Files|*.*|我要显示的文件类型（*.exe）|*.exe";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.FilterIndex = 1;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.textBoxAPDLFile.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void buttonResultWorkDic_Click(object sender, EventArgs e)
+        {
+            //string localFilePath, fileNameExt, newFileName, FilePath;  
+            string FilePath = String.Empty;
+            string localFilePath = String.Empty;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            //设置文件类型  
+            saveFileDialog1.Filter = " txt files(*.txt)|*.txt|All files(*.*)|*.*";
+            //设置文件名称：
+            saveFileDialog1.FileName = this.textBoxResultFile.Text.ToString();
+
+            //设置默认文件类型显示顺序  
+            saveFileDialog1.FilterIndex = 1;
+
+            //保存对话框是否记忆上次打开的目录  
+            saveFileDialog1.RestoreDirectory = true;
+
+            //点了保存按钮进入  
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //获得文件路径  
+                localFilePath = saveFileDialog1.FileName.ToString();
+                //string filname = this.openFileDialog2.FileName;
+                //获取文件名，不带路径  
+                //fileNameExt = localFilePath.Substring(localFilePath.LastIndexOf("\\") + 1);  
+
+                //获取文件路径，不带文件名  
+                FilePath = localFilePath.Substring(0, localFilePath.LastIndexOf("\\"));  
+
+                //给文件名前加上时间  
+                //newFileName = DateTime.Now.ToString("yyyyMMdd") + fileNameExt;  
+
+                //在文件名里加字符  
+                //saveFileDialog1.FileName.Insert(1,"dameng");  
+
+                //System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();//输出文件  
+
+                //fs输出带文字或图片的文件，就看需求了  
+                this.textBoxOutputDic.Text = FilePath;
+            }
+        }
+
+        private void buttonExcuteAnsys_Click(object sender, EventArgs e)
+        {
+            string ansysPath = this.textBoxAnsysFile.Text;
+            string apdlFilePath = this.textBoxAPDLFile.Text;
+            string outFilePath = this.textBoxOutputDic.Text;
+            string jobName = this.textBoxJobName.Text;
+            string outFilePathName = outFilePath + "\\" + this.textBoxResultFile.Text.ToString() + ".txt"; 
+
+            if (apdlFilePath != "")
+            {
+                try
+                {
+                    ProcessStartInfo startinfo = new ProcessStartInfo();
+                  //  startinfo.Arguments = "-b list -p ansysds -runae -j temp -i " + filePath + " -o e:/out.dat";
+                    string commond = String.Empty;
+                    commond += "-b -p ";  //in batch mode  Defines which ANSYS product will run during the session (ANSYS Multiphysics, ANSYS Structural, etc.)
+                    commond += "-j " + jobName; //jobName
+                    commond += " -np " + 2.ToString();//Specifies the number of processors to use when running Distributed ANSYS or Shared-memory ANSYS
+                    commond += " -db "; //Defines the portion of workspace (memory) to be used as the initial allocation for the database. This and -m are the two most important options. If you ever find that ANSYS is writing a *.PAGE file, up this number.
+                    commond += " -m "; //Idefines the total memory to reserve for the program. It is always better to reserve it up front rather than letting ANSYS grab as it needs.
+                    commond += " -i " + apdlFilePath;// 输入文件
+                    commond += " -o " + outFilePathName; // 输出文件                  
+                    startinfo.Arguments = commond;
+
+                    //startinfo.Arguments = "-b -p -j " + jobName + " -i " + apdlFilePath + " -o " + outFilePath + this.textBoxResultFile + ".txt";
+                //    startinfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    startinfo.WindowStyle = ProcessWindowStyle.Normal;
+                 //   startinfo.CreateNoWindow = true;
+                    //      startinfo.RedirectStandardOutput = true;
+                    startinfo.FileName = ansysPath;
+                    startinfo.WorkingDirectory = System.IO.Path.GetDirectoryName(outFilePathName);
+                    System.Diagnostics.Process ProAnsys = System.Diagnostics.Process.Start(startinfo);
+                    ProAnsys.WaitForExit();
+                    ProAnsys.Close();
+                    string path = @"C:\Users\Cratza\Desktop\C#\ANSYS_CSharp_Coding\ANSYS_CSharp_Coding\bin\Debug\ANSYS\demo.dat";
+                    StreamReader sr = new StreamReader(path, Encoding.Default);
+                    String line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Debug.WriteLine(line.ToString());
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("错误！");
+                }
+            }
+        }
+
+    
+
+
+
+
+
+
+    }
+
+    
+
+  
+
+
+   
+}
